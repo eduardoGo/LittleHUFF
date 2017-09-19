7a7bafd6572ef1ada7982d38595c84f326a3bd7e
Danilo Fernandes <danilofernandes45@gmail.com>
2017-09-19 12:24:08 -0300

    Update binary_tree.c

--- binary_tree.c
+++ binary_tree.c
@@ -1,10 +1,8 @@
-//#include "binary_tree.h"
+#include "binary_tree.h"
+#include "hash_table.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-typedef struct BINARY_TREE binary_tree;
-
-struct BINARY_TREE
+struct _binary_tree
 {
 	void *item;
 	int priority;
@@ -27,38 +25,20 @@ binary_tree* create_binary_tree(void *item, int priority, binary_tree *next, bin
 	bt->next = next;
 	bt->left = left;
 	bt->right = right;
+
 	return bt;
 }
 
-
-void print_queue(binary_tree *bt)
-{
-	binary_tree *current = bt;
-	while(current != NULL)
-	{
-		printf("Item: '%c', frequencia: %d\n", *(char*)current->item,current->priority);
-		current = current->next;
-	}
-}
-
-
 binary_tree* enqueue(binary_tree *bt, binary_tree *new_element)
 {
-	if(bt == NULL)
+	if(bt == NULL || bt->priority >= new_element->priority)
 	{
 		new_element->next = bt;
 		bt = new_element;
 
-	}
-	else if(bt->priority >= new_element->priority)
-	{
-		new_element->next = bt;
-		bt = new_element;
-	}
-	else
-	{
+	} else{
 		binary_tree *current = bt;
-		while(current->next != NULL && bt->next->priority < new_element->priority)
+		while(bt->next != NULL && bt->next->priority < new_element->priority)
 		{
 			current = current->next;
 		}
@@ -66,7 +46,7 @@ binary_tree* enqueue(binary_tree *bt, binary_tree *new_element)
 		new_element->next = current->next;
 		current->next = new_element;
 	}
-	print_queue(bt);//Só pra verificar como ta a fila
+
 	return bt;
 
 }
@@ -78,10 +58,62 @@ binary_tree* queue_to_tree(binary_tree *bt)
 	{
 		binary_tree *next = bt->next;
 		int priority = bt->priority + next->priority;
-		binary_tree *parent = create_binary_tree((char*) '*', priority, bt, next, NULL);
+
+		unsigned char *flag = (unsigned char *) malloc(sizeof(unsigned char));
+		*flag = '*';
+
+		binary_tree *parent = create_binary_tree(flag, priority, bt, next, NULL);
 		return queue_to_tree( enqueue(next->next, parent) );
 
 	} else {
 		return bt;
 	}
+}
+
+int hash_function(void *key)
+{
+	return *( (int*) key );
+}
+
+int equals(void *key1, void *key2)
+{
+	return ( *(int*) key1 ) == *( (int*) key2 );
+}
+
+unsigned char set_bit(unsigned char c, int i)
+{
+	unsigned char mask = 1 << i;
+	return c | mask;
+}
+
+int is_bit_i_set(unsigned char c, int i)
+{
+	unsigned char mask = 1 << i;
+	return c & mask;
+}
+
+void mapping(binary_tree *bt, hash_table *ht, unsigned char code, int depth)
+{
+	if(bt->left == NULL && bt->right == NULL)
+	{
+		unsigned char *value = (unsigned char *) malloc(sizeof(unsigned char));
+		*value = code;
+
+		put(ht, bt->item, value, hash_function, equals);
+	
+	} else {
+
+		mapping(bt->left, ht, code, depth + 1);
+		code = set_bit(code, 8 - depth);
+		mapping(bt->right, ht, code, depth + 1);
+
+	} 
+}
+
+hash_table* tree_to_table(binary_tree *bt)
+{
+	hash_table *ht = create_hash_table();
+	mapping(bt, ht, 0, 0);
+
+
 }
