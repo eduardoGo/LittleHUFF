da6c3d64727c3516e0611fa702d05c1776b882c0
Victor Matheus <victor.matheus.xd@gmail.com>
2017-09-20 13:40:00 -0300

    descompress 0.01

--- descompress.c
+++ descompress.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "binary_tree.h"
 #include "compress.h"
 #include "hash_table.h"
 
 #define MAX_SIZE 256
+
+char * get_binary(char byte)
+{
+	int a = byte;
+	char b[8];
+	itoa(a,b,2);
+
+	return b;
+}
+
+int get_trash(char bytes[2])
+{
+	
+}
+
+binary_tree * get_tree();
