becc6d048ea94976af8b3903be92903cdb66583f
eduardoGo <egsantos128@gmail.com>
2017-09-21 11:55:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "compress.h"
+#include "descompress.h"
 
 int main(int argc, char const *argv[])
 {
 	FILE *arq;
-	char a[100];
+	FILE *new_arq;
+	char name[200];
+	long int tam = 0;
+
 	if(argc == 3)
 	{
 		if(argv[1][0] == 'c')
 		{
 			arq = fopen(argv[2],"rb");
-			fgets(a,100,arq);
-			printf("%s\n", a);
+			if(arq == NULL)
+			{
+				printf("Não foi possível abrir o arquvio\n");
+				return 0;
+			}
+			printf("Digite o nome do arquivo de saída:\n");
+			fgets(name,198,stdin);
+			strcat(name,".huff");
+			new_arq = fopen(name, "w");
+			fseek(arq,0,SEEK_END);
+			tam = ftell(arq);
+			fseek(arq,0,SEEK_SET);
+			printf("Arquivo com %li bytes!\n", tam);
+			compress(new_arq,arq,tam);
 		}
 		else if(argv[1][0] == 'd')
 		{
 			arq = fopen(argv[2],"rb");
 			printf("DSucess\n");
 		}
+		else
+		{
+			printf("Argumentos inválidos, ultilize o formato (< c > or < d >) < file >");
+			return 0;
+		}
 	}
 	else
 	{
-		printf("Argumentos inválidos, ultilize o formato < c > < file >");
+		printf("Argumentos inválidos, ultilize o formato (< c > or < d >) < file >");
 		return 0;
 	}
 	return 0;
