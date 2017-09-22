d020c18707f7658a84a69748f1d24d023a06f586
eduardoGo <egsantos128@gmail.com>
2017-09-22 20:37:33 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "compress.c" //dps tem que mudar isso
+#include <string.h>
+#include "compress.c"
 
 int main(int argc, char const *argv[])
 {
 	FILE *arq;
+	FILE *new_arq;
+	char name[200];
 	long int tam = 0;
 
 	if(argc == 3)
@@ -17,21 +20,31 @@ int main(int argc, char const *argv[])
 				printf("Não foi possível abrir o arquvio\n");
 				return 0;
 			}
+			printf("Digite o nome do arquivo de saída:\n");
+			fgets(name,198,stdin);
+			strcat(name,".huff");
+			new_arq = fopen(name, "w");
 			fseek(arq,0,SEEK_END);
 			tam = ftell(arq);
 			fseek(arq,0,SEEK_SET);
 			printf("Arquivo com %li bytes!\n", tam);
-			compress(arq,tam);
+
+			compress(new_arq,arq,tam);
 		}
 		else if(argv[1][0] == 'd')
 		{
 			arq = fopen(argv[2],"rb");
 			printf("DSucess\n");
 		}
+		else
+		{
+			printf("Argumentos inválidos, ultilize o formato (< c > or < d >) < file >");
+			return 0;
+		}
 	}
 	else
 	{
-		printf("Argumentos inválidos, ultilize o formato < c > < file >");
+		printf("Argumentos inválidos, ultilize o formato (< c > or < d >) < file >");
 		return 0;
 	}
 	return 0;
