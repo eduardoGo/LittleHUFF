84016480200358c38500c3cf59ac444d0f48caa0
eduardoGo <egsantos128@gmail.com>
2017-09-24 21:07:26 -0300

    Add files via upload

---
+++ inc/compress.h
@@ -0,0 +1,12 @@
+#ifndef COMPRESS_H
+#define COMPRESS_H
+
+#include <stdio.h>
+
+//RECEBE UM ARQUIVO VAZIO,O ARQUIVO QUE VAI SER COMPACTADO E O TAMANHO DO
+//ARQUIVO QUE VAI COMPACTADO. COMPACTA O ARQUIVO heueh
+
+void compress(FILE *new_arq,FILE *arq, int tamanho);
+
+
+#endif
