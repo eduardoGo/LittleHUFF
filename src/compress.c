f917708e25db2fb7f04df5148102e27e1f5c1f01
eduardoGo <egsantos128@gmail.com>
2017-09-24 21:05:11 -0300

    Update compress.c

--- src/compress.c
+++ src/compress.c
@@ -1,5 +1,7 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../inc/binary_tree.h"
 #include "../inc/hash_table.h"
 #include "../inc/compress.h"
@@ -19,7 +21,10 @@ int* frequency(FILE *arq, int tam)
 	return freq;
 }
 
-
+int compara(void* key1,void* key2)
+{
+	return *((char*) key1) == *((char*) key2);
+}
 
 int hash_fuction(void *key)
 {
@@ -27,29 +32,21 @@ int hash_fuction(void *key)
 }
 
 
-char add_byte(unsigned char byte, int pos)
+char add_bit(unsigned char byte, int pos)
 {
-	unsigned mask = 1 << (7 - pos);
+	unsigned char mask = 0;
+	mask = 1 << (7 - pos);
 	return mask | byte;
 }
 
-void set_trash(FILE *new_arq,unsigned char trash)
+void set_header(FILE *new_arq,short int tree_size, unsigned char trash)
 {
-	unsigned char byte;
-	byte = getc(new_arq);
-	trash = trash << 5;
-	byte = byte & trash;
-	fprintf(new_arq,"%c",byte);
-}
-
-
-void set_tree_size(FILE *new_arq,short int tree_size)
-{
-	unsigned char byte1 = (tree_size & 255);
-	unsigned char byte2 = ((tree_size >> 8) & 255);
-	fprintf(new_arq, "%c", byte2);
-	fprintf(new_arq, "%c", byte1);
-
+	unsigned char byte = tree_size >> 8;
+	trash =  trash | byte;
+	fprintf(new_arq, "%c", trash);
+	byte = tree_size;
+	trash = byte & 255; //reaproveitando a variavel trash
+	fprintf(new_arq, "%c", trash);
 }
 
 
@@ -65,52 +62,54 @@ void set_tree(FILE *new_arq, char *tree)
 void codding(FILE *new_arq, FILE *arq, hash_table *dicionary,int size_arq,binary_tree *bt)
 {
 	int i,j,pos;
-
 	unsigned char byte = 0,trash = 0;
+	
 	char *current,*tree;
-	int *tree_size = (int*) malloc(sizeof(int));
+	short int tree_size;
 	
 	fprintf(new_arq,"%c", byte);
 	fprintf(new_arq,"%c", byte);
 
-	tree = tree_pre_order(bt,tree_size); //Ponteiro para uma string que contem a arvore em pre ordem
-										  // tambem coloca o tamanho da arvore no ponteiro *tree_size
-	
+	tree = traversal_tree(bt); //Ponteiro para uma string que contem a arvore em pre ordem
+	tree_size = strlen(tree);
+
 	set_tree(new_arq,tree); //Coloca a arvore em pre ordem no arquivo
-	fseek(new_arq,*tree_size,SEEK_CUR); //Seta o fluxo corrente a partir da arvore
-
 	
 	pos = j = i = 0;
 
 	for(j = 0; j < size_arq; ++j)
 	{
+		//printf("< Rodando o arquivo no byte %d>\n", j);
 		unsigned char *aux = (unsigned char*) malloc(1*sizeof(unsigned char));
 		*aux = getc(arq);
-		current = (unsigned char*) get(dicionary, aux, hash_fuction);
+		current = (unsigned char*) get(dicionary, aux, hash_fuction, compara);
+		//printf("Caracter: %x codificacao a ser gravada: %s\n", *aux,current);
 		while(current[i] != '\0')
 		{
 			if(pos == 8)
 			{
 				fprintf(new_arq,"%c", byte);
+				//printf("Gravou byte: %x\n", byte);
 				byte = 0;
 				pos = 0;	
 			}
-			if(current[i] == '1') byte = add_byte(byte,pos);
-
+			if(current[i] == '1') byte = add_bit(byte,pos);
 			pos++;
 			i++;
 		}
-	}
-	
-	fseek(new_arq,0,SEEK_SET); //Seta o fluxo corrente no inicio do arquivo
-	set_tree_size(new_arq,*tree_size);
-	
-	if(pos != 0)
-	{
-		trash = 8 - pos; //trash contem a quantidade de bits que nao estao em uso no ultimo byte do arquivo
-		set_trash(new_arq,trash);
+		i = 0;
 	}
 
+	printf("Size tree: %d\n", tree_size);
+	fprintf(new_arq, "%c", byte);
+	printf("Trash: %d\n", 8-pos);
+	fseek(new_arq,0,SEEK_SET);
+
+
+	trash = (8-pos) << 5;
+	set_header(new_arq,tree_size,trash);
+
+	fseek(new_arq,0,SEEK_SET);
 
 }
 
@@ -119,16 +118,25 @@ void codding(FILE *new_arq, FILE *arq, hash_table *dicionary,int size_arq,binary
 void compress(FILE *new_arq,FILE *arq, int tamanho)
 {
 	printf("Take frequency...\n");
-	
 	int *freq = frequency(arq,tamanho);
 	int i;
 	binary_tree *bt = create_empty_binary_tree();
 	hash_table *dicionary;
 
-	for(i = 0; i < MAX_SIZE; ++i) printf("%d ", freq[i]); //printa o array de frequencia
-	printf("\n\n");
+	int max = 100,a = 0;
+	unsigned char b,d;
+	char *teste;
 	
-	
+	for(i = 0; i < MAX_SIZE; ++i)
+	{
+		if(freq[i] < max)
+		{
+			a = max;
+			max = freq[i];
+			d = b;
+			b = i;
+		}
+	}
 
 	for(i = 0; i<MAX_SIZE; ++i)
 	{
@@ -136,20 +144,27 @@ void compress(FILE *new_arq,FILE *arq, int tamanho)
 		{
 			int *code = (int *) malloc(sizeof(int));
 			*code = i;
-			bt = enqueue(bt,code,freq[i]);
+			bt = enqueue(bt,
+				create_binary_tree(code,freq[i],NULL,NULL,NULL));
 		}
 	}
-
 	printf("Creatting tree...\n");
 	bt = queue_to_tree(bt);
 	printf("Creatting dicionary...\n");
 	dicionary = tree_to_table(bt);
 	printf("Codding...\n");
+
+	printf("Byte de menor frequencia: %x\n", b);
+	unsigned char *aux1 = (unsigned char*) malloc(1*sizeof(unsigned char));
+	*aux1 = b;
+	teste = (unsigned char*) get(dicionary, aux1, hash_fuction, compara);
+	printf("Codificalcao do byte de menor frequencia: %s\n", teste);
 	codding(new_arq,arq,dicionary,tamanho,bt);
 	
 	printf("Sucess! :)\n");
 	
-	
+	fclose(new_arq);
+	fclose(arq);
 
 
 }
